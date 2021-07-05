7a4a05c4ba93a7bb69feac45d5f55aa49e00f938
Mohammad Azam <azamsharp@gmail.com>
2021-07-04 20:10:01 -0500

    Initial Commit

---
+++ LearnTouchID/LearnTouchIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTouchIDApp.swift
+//  LearnTouchID
+//
+//  Created by Mohammad Azam on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnTouchIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
